3abec3583ebec7c4f990726bfb6be9e94f5e132a
pirosiki197 <lingmukangtai825@gmail.com>
2025-09-05 09:29:26 +0900

    fix

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -6,5 +6,6 @@ mkdir -p .built
 
 for directory in overlay/*; do
     echo "Building $directory..."
+    mkdir -p .built/built
     kustomize build "$directory" > .built/$(echo "$directory").yaml
 done
