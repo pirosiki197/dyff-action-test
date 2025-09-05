69d8c7d3c4b9461bbeaf932e4670886d902a63f3
pirosiki197 <lingmukangtai825@gmail.com>
2025-09-05 09:36:35 +0900

    動くはず

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -3,7 +3,6 @@
 set -euo pipefail
 
 mkdir -p .built/built
-ls -lA
 for directory in overlay/*; do
     echo "Building $directory..."
     kustomize build "$directory" > .built/$(echo "$directory").yaml
