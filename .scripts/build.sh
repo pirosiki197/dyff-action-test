97daca24d439b6a1a406a63821e194f3f2feb4c8
pirosiki197 <lingmukangtai825@gmail.com>
2025-09-05 09:49:12 +0900

    a

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -5,5 +5,7 @@ set -euo pipefail
 mkdir -p .built/overlay
 for directory in overlay/*; do
     echo "Building $directory..."
-    kustomize build "$directory" > .built/$(echo "$directory").yaml
+    kustomize build "$directory" --load-restrictor LoadRestrictionsNone --enable-helm \
+        | yq ".metadata.namespace = (.metadata.namespace // \"$directory\")" \
+        > .built/$(echo "$directory").yaml
 done
