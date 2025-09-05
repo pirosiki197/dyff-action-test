73c8c265f0cfbe267a8f55358e8341c23946f1c3
pirosiki197 <lingmukangtai825@gmail.com>
2025-09-05 09:45:01 +0900

    fix

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-mkdir -p .built/built
+mkdir -p .built/overlay
 for directory in overlay/*; do
     echo "Building $directory..."
     kustomize build "$directory" > .built/$(echo "$directory").yaml
